335e5f06f56bd92e79f21ba96c695f9ebc7bca52
TheMadDragon <91653031+TheMadDragon@users.noreply.github.com>
2022-12-15 10:25:55 +0530

    Add files via upload

---
+++ Day 16.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+     public static void main(String []args){
+         Scanner scan = new Scanner(System.in);
+         try {
+             String a = scan.next();
+             System.out.println(Integer.parseInt(a));
+         }catch(Exception e){
+             System.out.println("Bad String");
+         }
+     }
+    
+}
